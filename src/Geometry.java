9dfa0834b6819eeff1bdeacb1797e7cab7c6acd0
leahewers <leahewers@172.16.21.97>
2017-12-16 00:30:16 -0500

    Leah's quiz commit

--- src/Geometry.java
+++ src/Geometry.java
@@ -1,9 +1,20 @@
 
 public class Geometry {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main (String [] args) {
+
+		Rectangle rect1 = new Rectangle();
+
+		Rectangle rect2 = new Rectangle(12.5, 11);
+
+		rect2.setHeight(-10);
+
+		System.out.println("Rectangle 1: " + rect1 + " has an area of " + rect1.getArea() + " sq ft");
+
+		System.out.println("Rectangle 2: " + rect2 + " has an area of " + rect2.getArea() + " sq ft");
+
+		}
 
 	}
 
-}
+
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,48 @@
 
 public class Rectangle {
+	private double width; 
+	private double height; 
 
+	public Rectangle () {
+		width= 10; 
+		height= 10; 
+	}
+	public Rectangle (double w, double h) {
+		width = w; 
+		height= h; 
+	}
+	public void setWeight(double w) {
+		if (w> 0) { 
+			width= w; 
+		}
+		else {
+			System.out.println("Width must be positive. Width not set");
+		}
+	}
+	public void setHeight (double h) {
+		if (h>0) {
+			height= h; 
+		}
+		else {
+			System.out.println("Height must be positive. Height not set");
+		}
+	}
+	
+	public double getWidth () {
+		return width; 
+	}
+	public double getHeight() { 
+		return height;
+	}
+	public double getArea() {
+		double area = width * height; 
+		return area;
+	}
+	public String toString() {
+		String Rectangle = width + "by " + height + "rectangle"; 
+		return Rectangle; 
+	}
 }
+	
+	
+	
